6c1936abf72c719e9101344ed9d2fdc0801ba948
DebdutBiswas <rupam.biswas@outlook.com>
2022-01-07 10:13:15 +0530

    Fixed Models

---
+++ models/users.js
@@ -0,0 +1,83 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Users', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: "username"
+    },
+    password: {
+      type: DataTypes.STRING(65),
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
+    },
+    role: {
+      type: DataTypes.TINYINT.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: 'Roles',
+        key: 'id'
+      }
+    },
+    reg_by: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: true
+    },
+    reg_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    tableName: 'Users',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "username",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "username" },
+        ]
+      },
+      {
+        name: "id",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "Role_Attribute_On_Roles",
+        using: "BTREE",
+        fields: [
+          { name: "role" },
+        ]
+      },
+    ]
+  });
+};
