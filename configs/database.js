6a75082776adaddee4ba03709bd0d432b0b07080
DebdutBiswas <rupam.biswas@outlook.com>
2022-01-08 00:26:17 +0530

    Updated Database Connection Imports

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const { readFileSync } = require('fs');
 const { Sequelize } = require('sequelize');
 
-const { dbHost, dbPort, dbType, dbUser, dbPassword, dbDatabase } = JSON.parse(fs.readFileSync('./configs/dbCreds.json'));
+const { dbHost, dbPort, dbType, dbUser, dbPassword, dbDatabase } = JSON.parse(readFileSync('./configs/dbCreds.json'));
 
 module.exports = new Sequelize(dbDatabase, dbUser, dbPassword, {
   host: dbHost,
